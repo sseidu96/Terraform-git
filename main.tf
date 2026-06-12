4ca00ce80f960cb7e487f12f4955b7120a93ee97
Safiatu Seidu <sseidu96>
2026-06-11 23:55:10 -0400

    ticket 1

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_lightsail_instance" "gitlab_test" {
   name              = "custom_gitlab"
   availability_zone = "us-east-1a"
   blueprint_id      = "amazon_linux_2"
-  bundle_id         = "nano_3_0"
+  bundle_id         = "nano_1_0"
 
   # key_pair_name = "livekey"
 
